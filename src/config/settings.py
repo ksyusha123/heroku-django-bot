b70dff66e083cda65c326e8fcf1f9f08d86058b8
Ksyusha <astakhova.ksyusha.07@mail.ru>
2022-02-12 22:13:45 +0500

    csrf cookie domain

--- src/config/settings.py
+++ src/config/settings.py
@@ -57,11 +57,13 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CSRF_COOKIE_DOMAIN = 'heroku-django-bot.herokuapp.com'
 
 ROOT_URLCONF = "config.urls"
 
