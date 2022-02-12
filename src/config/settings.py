eb2fd1b78257d36c1e4acd63f1f1cc70a9c7aa6b
Ksyusha <astakhova.ksyusha.07@mail.ru>
2022-02-12 22:07:33 +0500

    debug true

--- src/config/settings.py
+++ src/config/settings.py
@@ -32,8 +32,8 @@ BOT_TOKEN = env("BOT_TOKEN")
 # BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "heroku-django-bot.herokuapp.com"]
 
